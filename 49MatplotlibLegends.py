8921e0202950e0e9ea4d64d53fe5410c00ef8508
amit <pundrikmishra1234@gmail.com>
2020-04-15 00:30:16 +0530

    14-05-2020

---
+++ 49MatplotlibLegends.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+#style.use('ggplot')
+#style.use('dark_background')
+#style.use('grayscale')
+
+
+# x = [5,6,7,8,0]
+x = [5,6,7,8]
+y = [7,3,8,3]
+x2= [5,6,7,8]
+y2= [6,7,2,6]
+
+# print(len(x))
+# print(len(y))
+
+plt.title('Epic Chart')
+plt.ylabel('Y axis')
+plt.xlabel('X  axis')
+
+plt.grid(True, color='g')
+
+plt.plot(x,y,'g',linewidth=5, label='Line One')
+plt.plot(x2,y2,'c',linewidth=10, label='Line Two')
+
+plt.legend()
+plt.show()
